e7f5366a1d4d92824d3ddba5ee4c87527e6dd2d9
suryaemqube <58320703+suryaemqube@users.noreply.github.com>
2026-07-09 17:48:32 +0530

    Update gtm code from GTM-54SH2H3 to GTM-KFWMDJVQ

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -39,7 +39,7 @@ export const onClientEntry = () => {
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-54SH2H3');
+      })(window,document,'script','dataLayer','GTM-KFWMDJVQ');
     `;
     document.head.appendChild(gtmInline);
 
